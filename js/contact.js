1cb60724e1cd695bf0444e30a9e4435a067062ab
Yan Hong <onlyhongyan@gmail.com>
2016-03-20 13:29:31 -0700

    Integrate Firebase for contact

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,20 @@
+var myDataRef = new Firebase('https://gf727yr3y0a.firebaseio-demo.com/contact_list');
+
+
+myDataRef.on('child_added', function(snapshot) {
+  //We'll fill this in later.
+  var contactObj = snapshot.val();
+  $('<p>').text('name: ' + contactObj.name).appendTo('body');
+  $('<p>').text('email: ' + contactObj.email).appendTo('body');
+  $('<p>').text('message: ' + contactObj.message).appendTo('body');
+});
+
+// console.log(myDataRef);
 $('#submit-btn').click(function() {
   var name = $('#name').val();
   var email = $('#email').val();
   var message = $('#message').val();
-  console.log('clicked');
+
   // $.get( "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo", function( data ) {
   //   console.log(data);
   // });
@@ -11,6 +23,12 @@ $('#submit-btn').click(function() {
   //   console.log(data);
   // });
 
+  var contactObj = {
+    name: name,
+    email: email,
+    message: message
+  }
 
-
+  myDataRef.push(contactObj);
 });
+
