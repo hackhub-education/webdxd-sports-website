c78fbebb33e0571b28023bb8e9843de58b694523
Yan Hong <onlyhongyan@gmail.com>
2016-03-20 11:53:26 -0700

    Add ajax calls

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+$('#submit-btn').click(function() {
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var message = $('#message').val();
+  console.log('clicked');
+  // $.get( "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo", function( data ) {
+  //   console.log(data);
+  // });
+
+  // $.post('/', {name: name, email: email, message: message}, function( data ) {
+  //   console.log(data);
+  // });
+
+
+
+});
