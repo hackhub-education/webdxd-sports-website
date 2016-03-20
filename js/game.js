f4df797d57588f78b7096357f816ea0f3baef00a
Yan Hong <onlyhongyan@gmail.com>
2016-03-20 14:54:42 -0700

    add and remove game object

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    var gameRef = new Firebase('https://gf727yr3y0a.firebaseio-demo.com/game_list');
+
     $('#show-btn').click(function() {
         // console.log("show button clicked");
         $('.game-table').slideDown();
@@ -32,32 +34,55 @@ $(document).ready(function() {
         console.log(team);
     });
 
-    function appendTd(row, name) {
-        var value = $('#' + name).val();
-        if (value != '') {
-            $('<td>').text(value).appendTo(row);
-            $('#' + name).val('');
-            return true;
-        } else {
-            return false;
-        }
+    // function appendTd(row, name) {
+    //     var value = $('#' + name).val();
+    //     if (value != '') {
+    //         $('<td>').text(value).appendTo(row);
+    //         return true;
+    //     } else {
+    //         return false;   
+    //     }
         
-    }
+    // }
 
     $('#add-btn').click(function() {
-        var row = $('<tr>');
-        if (appendTd(row, 'date') 
-            && appendTd(row, 'time')
-            && appendTd(row, 'team1')
-            && appendTd(row, 'team2')) {
-            row.appendTo('#game-table');
+        if ($('#date').val() != "" 
+            && $('#time').val() != ""
+            && $('#team1').val() != ""
+            && $('#team2').val() != "") {
+
+            var gameObj = {
+                date: $('#date').val(),
+                time: $('#time').val(),
+                team1: $('#team1').val(),
+                team2: $('#team2').val()
+            }
+
+            gameRef.push(gameObj);
+
+            $('#date, #time, #team1, #team2').val('');
         } else {
             alert("Please check your input variables!");
         }
     });
 
     $('table').on('click', 'td', function() {
+        var gameKey = $(this).parent().attr('id');
+        // console.log(gameKey);
+        var currentGame = new Firebase('https://gf727yr3y0a.firebaseio-demo.com/game_list/' + gameKey);
+        currentGame.remove();
         $(this).parent().remove();
+    });
+
+    gameRef.on('child_added', function(snapshot) {
+      var gameObj = snapshot.val();
+      var gameKey = snapshot.key();
+      console.log(gameKey);
+      var row = $('<tr>').attr('id', gameKey).appendTo('#game-table');
+      $('<td>').text(gameObj.date).appendTo(row);
+      $('<td>').text(gameObj.time).appendTo(row);
+      $('<td>').text(gameObj.team1).appendTo(row);
+      $('<td>').text(gameObj.team2).appendTo(row);
     });
 
 }); 
